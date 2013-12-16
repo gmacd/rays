b8bd9308218903254e18bec8f5161d5906ab46b4
Graham MacDonald <grahamamacdonald@gmail.com>
2013-12-16 22:42:17 +0000

    Move light

---
+++ src/gmacd/geom/light.go
@@ -0,0 +1,39 @@
+package geom
+
+import (
+	"gmacd/core"
+)
+
+// TODO Lights as separate entities?
+// TODO Light details as part of material?
+
+type Light interface {
+	IsLight() bool
+	SetIsLight(isLight bool)
+	LightCentre() core.Vec3
+}
+
+type LightData struct {
+	isLight bool
+	pos     core.Vec3
+}
+
+func NewLightData(pos core.Vec3) *LightData {
+	return &LightData{false, pos}
+}
+
+func NewLightDataNone() *LightData {
+	return &LightData{false, core.NewVec3Zero()}
+}
+
+func (light *LightData) IsLight() bool {
+	return light.isLight
+}
+
+func (light *LightData) SetIsLight(isLight bool) {
+	light.isLight = isLight
+}
+
+func (light *LightData) LightCentre() core.Vec3 {
+	return light.pos
+}
--- src/gmacd/geom/primitives.go
+++ src/gmacd/geom/primitives.go
@@ -4,9 +4,6 @@ import (
 	"gmacd/core"
 )
 
-// TODO Lights as separate entities?
-// TODO Light details as part of material?
-
 type Primitive interface {
 	Name() string
 	SetName(name string)
@@ -19,12 +16,6 @@ type Shape interface {
 	Intersects(ray core.Ray, maxDist float64) (result int, dist float64)
 	Normal(v core.Vec3) core.Vec3
 	Material() *core.Material
-}
-
-type Light interface {
-	IsLight() bool
-	SetIsLight(isLight bool)
-	LightCentre() core.Vec3
 }
 
 type PrimitiveData struct {
@@ -41,29 +32,4 @@ func (primitive *PrimitiveData) Name() string {
 
 func (primitive *PrimitiveData) SetName(name string) {
 	primitive.name = name
-}
-
-type LightData struct {
-	isLight bool
-	pos     core.Vec3
-}
-
-func NewLightData(pos core.Vec3) *LightData {
-	return &LightData{false, pos}
-}
-
-func NewLightDataNone() *LightData {
-	return &LightData{false, core.NewVec3Zero()}
-}
-
-func (light *LightData) IsLight() bool {
-	return light.isLight
-}
-
-func (light *LightData) SetIsLight(isLight bool) {
-	light.isLight = isLight
-}
-
-func (light *LightData) LightCentre() core.Vec3 {
-	return light.pos
 }
